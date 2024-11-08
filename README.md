e6922d3caf4e0477687dc81f878e8c90b847ef6f
Dok <dok2d+git@yandex.ru>
2024-11-08 09:09:19 +0400

    SERVICE PACK. Add arguments, md5 check, checking free disk space and giveawayclaim

--- README.md
+++ README.md
@@ -1,34 +1,50 @@
 # Выгрузка всех игр из личной библиотеки GOG
 
-У меня за несколько лет использования GOG, накопилось больше ста игр.
-И однажды встал вопрос, как бы их разом сдампить. Залез на страницу, увидел API-вызовы и понеслось)
+С течением лет на платформе GOG у меня накопилось более ста игр, и однажды возник вопрос — как бы разом сдампить их все?
+Просмотрев API-вызовы на сайте, я решил создать скрипт для автоматизации этого процесса.
 
 ![Demonstration of running the script](/../../../../dok2d/assets/blob/master/gog-games-downloader-preview.jpg)
 
 ## Требования: jq, curl, wget
-Перед запуском необходимо заполнить в начале скрипта параметры:
-- platforms — список платформ, для которых нужно скачать игры. Например, lin, win, mac.
-- langs_priority — список языков, которые нужно скачать, в порядке приоритета. Например, deu, eng.
-- outpath — путь, куда будут скачаны игры. Например, /var/www.
 
-Далее, захватываем и сохраняем свою куку с www.gog.com в файл. Выглядит она, примерно, так
+Перед запуском необходимо захватить и сохранить свою куку с www.gog.com в файл. Выглядит она, примерно, так
 
 ```
 Cookie: csrf=true; gog_lc=RU_RUB_ru-RU; CookieConsent={stamp:%274W2xTRAtatatatatatatata+TRAtata+Nnoasdg==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cmethod:%27explicit%27%2Cver:1%2Cutc:749574957495%2Cregion:%27ru%27}; gog_us=OOOOOMoyaOboronaaaa; cart_token=aff749574957495f; gog-al=10O8XXXXXXXXXXXXXX-H3XXXXXXXXXXX-KoXXXXXXXXXXXX_XXXXXXXXXXXXXX-; front_ab=old
 ```
 
+Для её получения можно использовать инспекцию страницы gog.com.
+Вкладка Network, фильтр domain:api.gog.com и отмечаем XHR. Обновляем страницу.
+Из контекстного меню любого появившегося пакета -> "Copy as cURL" и там уже находим нужное поле.
+
 ## Запуск
-`bash gog-games-downloader.sh cookie_file`
+
+`bash gog-games-downloader.sh -с cookie_file`
+
+### Дополнительные аргументы
+
+- `-c` `--cookie-file` Путь до файла с cookie. Обязательный аргумент.
+- `-o` `--out-path` Путь куда скачивать. Если не указано, то `~/gog-dump`
+- `-p` `--platforms` Под какие ОС скачивать. Доступны lin win mac. Если не указано, то `rus eng`
+- `-l` `--langs-priority` Приоритет выбора языка. Если не указано, то `rus eng`
+- `--only-giveawayclaim` Только проверка бесплатной раздачи и выход
+
+### Примеры запуска
+
+`bash gog-games-downloader.sh -o cook_file -p "mac win" -l "fra eng"`
+`bash gog-games-downloader.sh -o cook_file --only-giveawayclaim`
 
 ### Особенности
+
 - Поддерживает 16 языков(больше в своей библиотеке не нашёл)
 - Скачает только первый найденный язык по указанным приоритетам
 - Если файл игры уже был скачан, то его скипнет
 - Если установщик игры составляет более одного файла, то кладёт в отдельную директорию
 - Если выдаётся cd-key игры, то он скачается тоже
+- Перед запросом списка игр, curl-запрос на https://www.gog.com/giveaway/claim
+- Сверка md5
+- Пропуск скачивания файла, если на диске недостаточно места
 
 ### TODO на будущее
-- Перед запросом списка игр, curl-запрос на https://www.gog.com/giveaway/claim
 - Добавить опциональную загрузку дополнительных материалов
 - Добавить проверку ранее скачанной предыдущей версии, если в gog обновили игру
-- Сверка md5 [[forum](https://www.gog.com/forum/general/verifying_integrity_of_downloaded_games)]
--- gog-games-downloader.sh
+++ gog-games-downloader.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 set -e
 
-#### EDIT THIS ####
-platforms=""
-langs_priority=""
-outpath=""
-
-### EXAMPLE
-#platforms="lin win mac" # for get all types of installation
-#langs_priority="deu eng" # If German lang isn't available - get English
-##Available lang in ISO 639-3: deu eng spa por tur fra ita pol ron fin swe ces rus zho jpn kor
-##
-##Run:bash get_gog_library <cook_file_from_gog.com>
-###################
-
-
-platforms="${platforms:-lin win}"
-langs_priority="${langs_priority:-rus eng}"
-outpath=${outpath:-/var/www}
-
 function exit_script {
   set +x
   unset err_stat
@@ -31,19 +13,103 @@ function exit_script {
   [ -n "${err_stat}" -a "${err_stat}" = yes ] && exit 1
 }
 
+t_help=$(echo -e "
+$(echo -e "Run: bash gog-games-downloader.sh -c [COOCKIE-FILE] [OPTIONS]
+Options:
+  Option œ© Long option œ© SubOption œ© Meaning
+  -c œ© --cookie-file œ© <file> œ© Required. Path to the cookie file
+  -o œ© --out-path œ© [PATH] œ© Path to save directory
+  -p œ© --platforms œ© [TYPE] œ© Select platforms. Default: lin win
+  -l œ© --langs-priority œ© [LANG] œ© List of languages to download, in order of priority. Default: rus eng
+     œ© --only-giveawayclaim œ©  œ© Check giveaway claim and exit
+
+Available arguments
+Platform types: lin win mac
+language in ISO 639-3: deu eng spa por tur fra ita pol ron fin swe ces rus zho jpn kor
+
+Examples
+bash gog-games-downloader.sh -c cook_file -p "mac win" -l "fra eng"
+bash gog-games-downloader.sh -c cook_file --only-giveawayclaim
+" | column -ets 'œ©')")
+
+while [ ! -z "${1}" ]; do
+  case "${1,,}" in
+    -c|--cookie-file) 
+      shift
+      [ -z "${1}" ] && exit_script err "The path to the cookie file is not specified!"
+      [ ! -f "${1}" ] && exit_script err "There is no cookie file named ${1}"
+      [ ! -s "${1}" ] && exit_script err "The file "${1}" is empty."
+      cook=$(cat "${1}") 
+      shift;;
+    -p|--platforms)
+      shift
+      [ -z "${1}" ] && exit_script err "Platforms is not specified!"
+      avail_platforms=("win" "lin" "mac")
+      while [[ $# -gt 0 && ! $1 =~ ^- ]]; do
+          [[ ! " ${avail_platforms[@]} " =~ " $1 " ]] && exit_script err "Platform ${1} is not in available list!"
+          platforms+="${1} "
+          shift
+      done;;
+    -l|--langs-priority)
+      shift
+      [ -z "${1}" ] && exit_script err "Languages is not specified!"
+      avail_languages=("deu" "eng" "spa" "por" "tur" "fra" "ita" "pol" "ron" "fin" "swe" "ces" "rus" "zho" "jpn" "kor")
+      while [[ $# -gt 0 && ! $1 =~ ^- ]]; do
+          [[ ! " ${avail_languages[@]} " =~ " $1 " ]] && exit_script err "Language ${1} is not in available list!"
+          langs_priority+="${1} "
+          shift
+      done;;
+    -o|--out-path)
+      [ -z "${1}" ] && exit_script err "The path to the cookie file is not specified!"
+      shift
+      outpath=${1}
+      shift;;
+    --only-giveawayclaim)
+      onlygiveawayclaim=yes
+      shift;;
+    -h|--help) exit_script info "${t_help}" ;;
+    *) exit_script info "Error flag - ${1}\n\n${t_help}" ;;
+  esac
+done
+
+platforms="${platforms:-lin win}"
+langs_priority="${langs_priority:-rus eng}"
+outpath=${outpath:-~/gog-dump}
+
 getpage() {
   [ -z "${1}" ] && exit_script err "NULL URL"
   curl -s -H "${cook}" "$(for i in $*; do echo -n "${i} "; done | sed 's/ $//g')"
 }
 
-[ -z "${1}" ] && exit_script err "The cookie file is not specified!"
-[ -f "${1}" ] && cook=$(cat "${1}") || exit_script err "Cookie file not found!"
+check_md5() {
+  echo -n " [md5:"
+  md5sum --quiet -c <(echo "$@") > /dev/null 2>&1
+  [ "$?" = 0 ] && echo "ok]" || echo "not ok]"
+}
+
+human_readable_filesize() {
+    local bytes=$1
+    local units=("B" "KB" "MB" "GB" "TB")
+    local i=0
+    while (( bytes >= 1024 && i < ${#units[@]} - 1 )); do
+        bytes=$(( bytes / 1024 ))
+        i=$(( i + 1 ))
+    done
+    printf "%.1f %s\n" "$bytes" "${units[i]}"
+}
+
+[ -z "${cook}" ] && exit_script err "The path to the cookie file is not specified!"
 [ "$(dpkg -l | awk '{print $2}' | grep -c "^jq$")" -ne 1 ] && apt-get --yes install jq
 totalpages="$(getpage "https://www.gog.com/account/getFilteredProducts?hiddenFlag=0&mediaType=1" | jq -r '.totalPages')"
 
 for i in $(seq 1 ${totalpages}); do
   inventory="$(echo -e "${inventory}${inventory:+\n}$(getpage "https://www.gog.com/account/getFilteredProducts?hiddenFlag=0&mediaType=1&page=${i}&sortBy=date_purchased" | jq -r '.products[] | "\(.id) \(.slug) \(.title)"')")"
 done
+
+[ -z "${onlygiveawayclaim}" ] && echo -e "Download dir: ${outpath}/ (Available$(df -h --output=avail "${outpath}" | tail -1))"
+getpage https://www.gog.com/giveaway/claim | jq -r '.message'
+echo
+[ "${onlygiveawayclaim}" = "yes" ] && exit_script
 
 echo -e "${inventory}" | grep -v "^$" | while read id slug name; do
   unset lang_selected
@@ -88,17 +154,33 @@ echo -e "${inventory}" | grep -v "^$" | while read id slug name; do
       if [[ "${platform_available}" == *${os_type}* ]]; then
         files_links=$(echo ${gameinfo} | jq -r '.downloads[] | select(.[0]=="'${lang_selected}'") | .[1] | .'${os_type}'[] | .manualUrl' | grep installer[0-9])
         files_count=$(echo -e "${files_links}" | wc -l)
-        [ "${files_count}" -eq 1 ] && outdir=${outpath}/GOG/${os} || outdir=${outpath}/GOG/${os}/${slug}
-        mkdir -p ${outdir}
-        [ -n "${cdkey}" ] && echo ${cdkey} > ${outdir}/${slug}.cdkey
+        [ "${files_count}" -eq 1 ] && outdir=${outpath}/${os} || outdir=${outpath}/${os}/${slug}
+        mkdir -p "${outdir}"
+        [ -n "${cdkey}" ] && echo ${cdkey} > "${outdir}/${slug}.cdkey"
         unset count_download
         for i in ${files_links}; do
           count_download=$((count_download+1))
           echo -n "${name} [${os}] (${count_download}/${files_count}).. "
           target_link=$(curl -sILH "${cook}" --write-out '%{url_effective}' --output /dev/null https://www.gog.com${i})
+          target_md5="$(getpage ${target_link}.xml | sed -n 's/.*md5="\([^"]*\)".*/\1/p')"
+          target_bytesize="$(getpage ${target_link}.xml | sed -n 's/.*total_size="\([^"]*\)".*/\1/p')"
           outfile_name=$(echo ${target_link} | sed 's/.*\///;s/%[0-9][0-9]//g;s/setup_//g;s/gog_//g;s///g;s/\.exe.*/.exe/g;s/\.bin.*/.bin/g')
-          wget ${target_link} -qcO ${outdir}/${outfile_name}
-          [ "$?" = 0 ] && echo ok || echo fail
+          if [ -f "${outdir}/${outfile_name}" ]; then
+            if [ $((${target_bytesize} - $(stat -c %s "${outdir}/${outfile_name}"))) -gt $(df --output=avail . | tail -1) ]; then
+              echo "Skipping a file that is too large. File size: $(human_readable_filesize ${target_bytesize}). Available disk space:$(df -h --output=avail "${outpath}" | tail -1)"
+              continue
+            fi
+          elif [ ${target_bytesize} -gt $(df --output=avail . | tail -1) ]; then
+            echo "Skipping a file that is too large. File size: $(human_readable_filesize ${target_bytesize}). Available disk space:$(df -h --output=avail "${outpath}" | tail -1)"
+            continue
+          fi
+          wget ${target_link} -qcO "${outdir}/${outfile_name}"
+          if [ "$?" = 0 ]; then
+            echo -n ok
+            check_md5 ${target_md5} "${outdir}/${outfile_name}"
+          else
+             echo fail
+          fi
         done
       fi
     done
